c167e3fdce5337736cc2d078f84f876f020af4cf
Kwathuta <kelvin.gitahi@student.moringaschool.com>
2021-09-27 19:38:59 +0300

    defining user login and sign in forms

---
+++ app/auth/forms.py
@@ -0,0 +1,48 @@
+from flask_wtf import FlaskForm
+from wtforms import (
+    StringField,
+    PasswordField,
+    BooleanField,
+    SubmitField,
+    ValidationError,
+)
+from wtforms.validators import DataRequired, Email, EqualTo
+from ..models import User
+
+
+class RegistrationForm(FlaskForm):
+
+    username = StringField(
+        "Your preferred username goes here", validators=[DataRequired()]
+    )
+
+    email = StringField(
+        "Your Email Address goes here", validators=[DataRequired(), Email()]
+    )
+
+    password = PasswordField(
+        "Password",
+        validators=[
+            DataRequired(),
+            EqualTo("password_confirm", message="Passwords must match"),
+        ],
+    )
+
+    password_confirm = PasswordField("Confirm Password", validators=[DataRequired()])
+
+    submit = SubmitField("Sign Up")
+
+    def validate_email(self, data_field):
+        if User.query.filter_by(email=data_field.data).first():
+            raise ValidationError("There is an account with that email")
+
+    def validate_username(self, data_field):
+        if User.query.filter_by(username=data_field.data).first():
+            raise ValidationError("That username is taken")
+
+
+class LoginForm(FlaskForm):
+    email = StringField("Your Email Address", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    remember = BooleanField("Remember me")
+    submit = SubmitField("Sign In")
