2890042ade35c6d1dc0ab5472b7059756a03b083
Kwathuta <kelvin.gitahi@student.moringaschool.com>
2021-09-27 19:09:30 +0300

    app initialized

---
+++ app/config.py
@@ -0,0 +1,45 @@
+import os
+
+
+class Config:
+    """
+    General parent configuration class
+    """
+
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = (
+        "postgresql+psycopg2://moringa:aljokela7247@localhost/devin_blog"
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    UPLOADED_PHOTOS_DEST = "app/static/photos"
+
+    MAIL_SERVER = "smtp.googlemail.com"
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+
+class ProdConfig(Config):
+    """
+    Production configuration child class
+    Args:
+        Config: The parent configuration class with Generl configuration settings
+    """
+
+    pass
+
+
+class DevConfig(Config):
+    """
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    """
+
+    DEBUG = True
+
+
+config_options = {"development": DevConfig, "production": ProdConfig}
