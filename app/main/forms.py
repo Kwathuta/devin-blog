73f6ffddbaed41b47dbdc5294973a4790559044f
Kwathuta <kelvin.gitahi@student.moringaschool.com>
2021-09-27 20:24:44 +0300

    defining forms on main.py

---
+++ app/main/forms.py
@@ -0,0 +1,30 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField, TextAreaField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField(
+        "Let people know a little bit more about you", validators=[DataRequired()]
+    )
+    submit = SubmitField("Save")
+
+
+class BlogForm(FlaskForm):
+    title = StringField("Title", validators=[DataRequired()])
+    category = SelectField(
+        "Category",
+        choices=[
+            ("Lifestyle", "Lifestyle"),
+            ("Fitness", "Fitness"),
+            ("Do It Yourself (DIY)", "Do It Yourself(DIY)"),
+        ],
+        validators=[DataRequired()],
+    )
+    post = TextAreaField("Your Blog", validators=[DataRequired()])
+    submit = SubmitField("Post")
+
+
+class CommentForm(FlaskForm):
+    comment = TextAreaField("Leave a comment", validators=[DataRequired()])
+    submit = SubmitField("Comment")
